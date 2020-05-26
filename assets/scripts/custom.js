6bb7d7ed2fba93cbe810a071b6b98e6b83445dac
Justas <jjustas.lukauskas@gmail.com>
2020-05-26 16:34:37 +0300

    .

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,19 +1,3 @@
-// const proxyurl = "https://cors-anywhere.herokuapp.com/";
-// const url = "https://example.com"; // site that doesn’t send Access-Control-*
-// fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
-//     .then((response) => response.text())
-//     .then((contents) => console.log(contents))
-//     .catch(() =>
-//         console.log("Can’t access " + url + " response. Blocked by browser?")
-//     );
-
-let api_url =
-    "https://thingproxy.freeboard.io/fetch/https://some-random-api.ml/facts/dog";
-$.ajax({
-    url: api_url,
-    contentType: "application/json",
-    dataType: "json",
-    success: function (result) {
-        console.log(result);
-    },
+$.getJSON("https://dog-api.kinduff.com/api/facts", function (data) {
+    console.log(data);
 });
